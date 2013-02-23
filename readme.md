38be5210a06cee0bb8b4c436222d2ba80d876862
hyspace <hyspace@gmail.com>
2013-02-23 17:44:15 +0800

    add dev

---
+++ readme.md
@@ -0,0 +1,109 @@
+### pull-to-refresh
+
+## quick view
+
+http://hyspace.cc/pull-to-refresh/
+
+## require
+
+zepto(0.8+) recommand
+
+or
+
+jQuery(1.8+) not tested yet
+
+## feature
+
+used css `-webkit-overflow-scrolling: touch;` so it's scrolls smoother than any lib using `-webkit-transform: translate3d`
+
+support Android 4+
+
+## broswer support
+
+- iOS 5+
+- Android 4+ (default browser)
+
+warn: exclude Chrome ver 18 or less on Android.
+
+tested in iOS6 and Android 4.1.2
+
+## usage
+
+### include `zepto.ScrollView.js` and `zepto.ScrollView.css`
+
+### include dom structure in html
+    
+    <!-- top structure of ScrollView -->
+    <div id='scroll-object' class="zui-scroll-view">
+        <div class="zui-scroll-view-top-loading">
+            <!-- show while loading -->
+            loading…
+            <!-- end -->
+        </div>
+          <div class="zui-scroll-view-main">
+            <div class='zui-scroll-view-warper'>
+              <div class='zui-scroll-view-scroller'>
+                <div class="zui-scroll-view-pull">
+                  <!-- show when pull -->
+                  pull to refresh
+                  <!-- end -->
+                </div>
+                <div class="zui-scroll-view-container">
+                  <!-- your content html here -->
+
+                  <!-- end of your content html -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <!-- end of ScrollView structure -->
+
+### add scrollview Object in script
+
+    scrollView = new $.ScrollView('#scroll-object');
+
+### add pull and finish listener
+    
+    scrollView.on('scrollview.pull',function (e) {
+        // do sth when pull
+
+        // hide the loading mark when task done.
+        scrollView.finishLoading();
+    })
+
+or listen to dom event of scrollObject.
+
+    $('#scroll-object').on('scrollview.pull',function (e) {
+        // do sth when pull
+
+        // hide the loading mark when task done.
+        scrollView.finishLoading();
+    })
+
+### manually control
+
+    // manually hide the loading mark (will fire the 'scrollview.finish' event.)
+    scrollView.finishLoading();
+
+    // manually start the loading progress (will fire the 'scrollview.pull' event.)
+    scrollView.startLoading();
+
+    // listen to finish event:
+    scrollView.on('scrollview.finish',function (e) {
+        // do sth when finish
+    })
+
+    // listen to finish event:
+    $('#scroll-object').on('scrollview.finish',function (e) {
+        // do sth when finish
+    })
+
+## further
+
+see demo
+
+## Lisence
+
+this project is licensed under the terms of the MIT License.
---
+++ src/zepto.ScrollView.js
@@ -0,0 +1,204 @@
+(function($) {
+    function addListener(el, type, listener, useCapture) {
+        el.addEventListener(type, listener, useCapture);
+        return { 
+            destroy: function() { el.removeEventListener(type, listener, useCapture); } 
+        };   
+    }
+    $.ScrollView = function(selector) {
+        // verify os and version.
+        // if ($.os.android && $.os.version.substring(0, 1) < 4) {
+        //     console.log('error: do not support Android 3-');
+        //     return;
+        // } else if ($.os.ios && $.os.version.substring(0, 1) < 5) {
+        //     console.log('error: do not support iOS 4-');
+        //     return;
+        // } else if (!$.os.ios && !$.os.android){
+        //     console.log('error: only support iOS 5+ and Android 4+');
+        //     return;
+        // }
+        //elements
+        this.el = {};
+        this.el.top = document.querySelector(selector);
+        this.el.root = this.el.top.querySelector('.zui-scroll-view-main');
+        this.el.topLoading = this.el.top.querySelector('.zui-scroll-view-top-loading');
+        this.el.warper = this.el.top.querySelector('.zui-scroll-view-warper');
+        this.el.scroller = this.el.top.querySelector('.zui-scroll-view-scroller');
+        if (!this.el.top || !this.el.root || !this.el.topLoading || !this.el.warper || !this.el.scroller) {
+            console.log('error: element[s] not found.');
+            return;
+        }
+
+        this.activeHeight = 0;
+        this.barHeight = this.el.topLoading.getBoundingClientRect().height;
+        this.activeHeight = this.el.top.getBoundingClientRect().height;
+
+        //stat parms
+        this.topActivated = false;
+        this.readyToActivate = false;
+
+        //add listeners
+        this.el.root.addEventListener('touchstart', this, false);
+        this.el.scroller.addEventListener('touchmove', this, false);
+        this.el.root.addEventListener('touchend', this, false);
+        this.el.root.addEventListener('touchcancel', this, false);
+        this.el.root.addEventListener('webkitTransitionEnd', this, false);
+
+
+        this.draging = {};
+        //stat parms only for android
+        this.draging.pre = 0;
+        this.listeners = [];
+    };
+    $.ScrollView.prototype = $.extend({
+        constructor: $.ScrollView,
+        finishLoading: function(argument) {
+            this.topActivated = false;
+            $(this.el.top).removeClass('top-activated');
+            $(this.el.top).addClass('top-animating');
+            this.el.topLoading.style.webkitTransform = 'translate3d(0,'+(-this.barHeight)+',0)';
+            this.el.root.style.webkitTransform = 'translate3d(0,0,0)';
+            $(this.el.root).height(this.activeHeight);
+            var evt = document.createEvent('Events')
+            evt.initEvent('scrollview.finish',false,false);
+            evt.targetScrollView = this;
+            this.el.top.dispatchEvent(evt);
+        },
+        startLoading:function(){
+            this.topActivated = true;
+            $(this.el.top).addClass('top-activated');
+            this.el.root.style.webkitTransform = 'translate3d(0,' + this.barHeight + 'px,0)';
+            var evt = document.createEvent('Events')
+            evt.initEvent('scrollview.pull',false,false);
+            evt.targetScrollView = this;
+            this.el.top.dispatchEvent(evt);
+        },
+        isLoading:function(){
+            return this.topActivated;
+        },
+        on:function (_eventName,fun) {
+            eventName = _eventName.toLowerCase();
+            if(typeof fun !== 'function')return;
+            switch(eventName){
+                case 'scrollview.pull':
+                case 'scrollview.finish':
+                    this.listeners.push(addListener(this.el.top,eventName,fun,false));
+                break;
+            }
+        },
+        destoryListeners:function () {
+            for(var i=0;i<this.listeners.length;i++){
+                this.listeners[i].destroy();
+            }
+        }
+    }, (navigator.userAgent.search('Android')!=-1) ? {
+        handleEvent: function(e) {
+            switch (e.type) {
+            case 'webkitTransitionEnd':
+                if (this.topActivated) {
+                    if (e.target == this.el.root) {
+                        $(this.el.root).height(this.activeHeight - this.barHeight);
+                    }
+                } else {
+                    $(this.el.top).removeClass('top-animating');
+                }
+                break;
+            case 'touchstart':
+                if(!this.topActivated && e.touches.length==1){
+                    this.activeHeight = this.el.top.getBoundingClientRect().height;
+                }
+                break;
+            case 'touchmove':
+                if (!this.topActivated) {
+                    if (!this.draging.isDraging && this.el.warper.scrollTop <= 0) {
+                        this.draging.pre++;
+                        if (this.draging.pre > 2) {
+                            this.draging.isDraging = true;
+                            this.draging.start = e.touches[0].screenY;
+                            this.draging.pre = 0;
+                            break;
+                        }
+                    }
+                    if (this.draging.isDraging) {
+                        e.preventDefault();
+                        this.draging.offset = e.touches[0].screenY - this.draging.start;
+                        var rubberOffset = this.draging.offset * 0.33;
+                        if (this.draging.offset <= 0) {
+                            this.draging.isDraging = false;
+                            this.el.scroller.style.webkitTransform = 'translate3d(0,0,0)';
+                            break;
+                        }
+                        if (rubberOffset > this.barHeight) {
+                            this.readyToActivate = true;
+                            $(this.el.top).addClass('top-activating');
+                            this.el.topLoading.style.webkitTransform = 'translate3d(0,'+(rubberOffset - this.barHeight)+'px,0)'
+                        } else {
+                            this.readyToActivate = false;
+                            $(this.el.top).removeClass('top-activating');
+                        }
+                        this.el.scroller.style.webkitTransform = 'translate3d(0,' + rubberOffset + 'px,0)';
+                    }
+                }
+                break;
+            case 'touchcancel':
+            case 'touchend':
+                $(this.el.top).removeClass('top-activating');
+                if (this.draging.isDraging) {
+                    if (this.readyToActivate) {
+                        this.startLoading();
+                    }
+                    this.draging.isDraging = false;
+                    $(this.el.top).addClass('top-animating');
+                    this.el.topLoading.style.webkitTransform = 'translate3d(0,0,0)';
+                    this.el.scroller.style.webkitTransform = 'translate3d(0,0,0)';
+                }
+                this.draging.pre = 0;
+                this.readyToActivate = false;
+                break;
+            }
+        }
+    } : {
+        handleEvent: function(e) {
+            switch (e.type) {
+            case 'webkitTransitionEnd':
+                e.stopPropagation();
+                if (this.topActivated && e.target == this.el.root) {
+                    $(this.el.root).height(this.activeHeight - this.barHeight);
+                }
+                $(this.el.top).removeClass('top-animating');
+                break;
+            case 'touchstart':
+                if(!this.topActivated && e.touches.length==1){
+                    this.activeHeight = this.el.top.getBoundingClientRect().height;
+                }
+                break;
+            case 'touchmove':
+                e.stopPropagation();
+                if (!this.topActivated && !!this.el.warper.scrollTop) {
+                    if (-this.el.warper.scrollTop > this.barHeight) {
+                        this.readyToActivate = true;
+                        this.draging.offset = -this.el.warper.scrollTop;
+                        this.el.topLoading.style.webkitTransform = 'translate3d(0,'+(this.draging.offset - this.barHeight)+'px,0)'
+                        $(this.el.top).addClass('top-activating');
+                    } else {
+                        this.readyToActivate = false;
+                        this.draging.offset = 0;
+                        $(this.el.top).removeClass('top-activating');
+                    }
+                }
+                break;
+            case 'touchend':
+                $(this.el.top).removeClass('top-activating');
+                if (this.readyToActivate) {
+                    console.log((this.draging.offset - this.barHeight));
+                    $(this.el.top).addClass('top-animating');
+                    this.el.topLoading.style.webkitTransform = 'translate3d(0,0,0)';
+                    this.startLoading();
+                }
+                this.readyToActivate = false;
+                this.draging.offset = 0;
+                break;
+            }
+        }
+    });
+})($);
